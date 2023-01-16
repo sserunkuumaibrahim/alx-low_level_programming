da5d9172471f0684383c2866de41fcc7f2719383
sserunkuumaibrahim <77914914+sserunkuumaibrahim@users.noreply.github.com>
2023-01-17 00:40:29 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #definde DOG_H
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
 typedef struct dog
 {
 	char *name;
