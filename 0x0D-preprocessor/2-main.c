4bb7c21942ca5db772546cc870667d6c77209ec6
sserunkuumaibrahim <77914914+sserunkuumaibrahim@users.noreply.github.com>
2023-01-17 01:53:02 +0300

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,11 @@
-#include 
+#include <stdio.h>
+/**
+ * main - Entry point, prints file name it was compiled from
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+} 
