4af24c93925845e64bfba133d84e87ff816e7733
sserunkuumaibrahim <77914914+sserunkuumaibrahim@users.noreply.github.com>
2023-01-17 00:39:32 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,15 @@
-#include "main.h"
+#ifndef DOG_H
+#definde DOG_H
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+/* Additional function prototypes */
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
