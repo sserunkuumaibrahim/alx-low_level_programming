282b002433cc4eb996d36697b3a1969fc56f4d1d
sserunkuumaibrahim <77914914+sserunkuumaibrahim@users.noreply.github.com>
2023-01-17 00:42:46 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,28 @@
-#ifndef DOG_H
-#definde DOG_H
+#ifndef DOG
+#define DOG
 /**
- * struct dog - dog data
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the owner of the dog
+ * struct dog - dog class
  *
- * Description: Defines metadata about the dog and type of dog's struct
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-/* Additional function prototypes */
-int _putchar(char c);
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
+
 #endif
