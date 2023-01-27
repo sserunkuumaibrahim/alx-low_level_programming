ced9db0934cf455719278799fe2af205c409d066
sserunkuumaibrahim <77914914+sserunkuumaibrahim@users.noreply.github.com>
2023-01-27 03:11:54 +0300

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,6 +1,10 @@
-#ifndef _CODING_H_
-#define _CODING_H_
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,7 +12,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Coding project
+ * for Holberton project
  */
 typedef struct list_s
 {
@@ -17,11 +21,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-char *_strdup(const char *str);
-#endif
+#endif /* LISTS_H */
