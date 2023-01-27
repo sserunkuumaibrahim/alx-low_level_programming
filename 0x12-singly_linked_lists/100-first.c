6acd4b18841127617748661d5a4a795b2915aa5e
sserunkuumaibrahim <77914914+sserunkuumaibrahim@users.noreply.github.com>
2023-01-27 05:05:44 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,2 +1,15 @@
-#`:wq
+#include <stdio.h>
 
+void firstfunct(void) __attribute__((constructor));
+
+/**
+ * firstfunct - print before the main function's execution
+ *
+ * Return: void
+ */
+
+void firstfunct(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
